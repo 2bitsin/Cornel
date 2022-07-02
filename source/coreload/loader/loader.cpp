ee2f9f6eb84c1961b61eee92b57cd8cd455c01b1
Comrade Alex <s.aleksandras@gmail.com>
2022-07-02 16:47:13 +0300

    Rebuild all build scripts

---
+++ source/coreload/loader/loader.cpp
@@ -0,0 +1,10 @@
+
+extern "C"
+{
+  __attribute__((__cdecl__))
+  auto _start() -> void
+  {
+
+  }
+}
+
--- source/coreload/stub/epilogue.s
+++ source/coreload/stub/epilogue.s
@@ -2,7 +2,13 @@
 .model tiny
 
 _EPILOGUE segment use16 public para 'EPILOGUE'
-  db 16 dup (1)
+
+  DB "CONFIGURATIONISM"
+
+  public proc _end_of_epilogue near
+_end_of_epilogue:
+
+  
 _EPILOGUE ends
 
 end
