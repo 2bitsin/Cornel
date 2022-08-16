6f8d88b1cda59a78668039ea4e28fe30491ebca1
Comrade Alex <s.aleksandras@gmail.com>
2022-08-16 04:00:19 +0300

    Bootp is completed for now

--- tools/build.bat
+++ tools/build.bat
@@ -1,4 +1,3 @@
-#!/bin/bash
 mkdir -p build
 mkdir -p bin
 pushd build
@@ -6,4 +5,3 @@ cmake .. -DCMAKE_BUILD_TYPE=Release
 cmake --build . --config Release
 cmake --install . --config Release
 popd
-
