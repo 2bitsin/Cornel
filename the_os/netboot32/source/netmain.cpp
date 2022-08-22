f9bc59aa261c135987a269e5e009b653eae98b2a
Comrade Alex <s.aleksandras@gmail.com>
2022-08-22 04:35:25 +0300

    Switching to PMODE

--- the_os/netboot32/source/netmain.cpp
+++ the_os/netboot32/source/netmain.cpp
@@ -1,12 +1,11 @@
-#include <stdint.h>
+#include <cstdint>
 
 uint16_t * const G_screen = (uint16_t*)0xB8000;
 
+extern "C"
 void NET_main (void)
 {
-  int i;
-
-  for (i = 0; i < 80*25; ++i) {
+  for (auto i = 0; i < 80*25; ++i) {
     G_screen[i] = 0x0700 + '#';
   }
 
--- the_os/netboot32/source/startup/startup.s
+++ the_os/netboot32/source/startup/startup.s
@@ -3,50 +3,79 @@
 
     org     0x7c00
     jmp     preamble
-    times   (16 - $ mod 16) nop
 
-disptbl:
-    dd      putstr
-    dd      putstr
-    dd      putstr
-    dd      putstr    
+strings:
+  .hello:
+    db      "NETBOOT32 v0.1 BUILD ", G_BUILD_TIMESTAMP, 13,10,0
 
+desctbl:
+
+  .begin:
+    dq 0x0000000000000000 ; 0x0000 = null
+    dq 0x00cf9e000000ffff ; 0x0008 = 32bit code 
+    dq 0x00cf92000000ffff ; 0x0010 = 32bit data/stack
+    dq 0x00009e000000ffff ; 0x0018 = 16bit code
+    dq 0x000092000000ffff ; 0x0020 = 16bit data/stack
+  .end:
+   
+    include "nmictl.s"
+    include "print.s"
 
 preamble:
+    pop     eax             ; Discard return address
+    pop     eax             ; Grab pointer to !PXE structure
+    
+    movzx   edx,    ax      ; edx = offset of !PXE structure
+    shr     eax,    16      ; eax = segment of !PXE structure
+    shl     eax,    4       ; eax = segment of !PXE structure * 16
+    add     eax,    edx     ; eax = linear address of !PXE structure
+    push    eax             ; Save pointer to !PXE structure
+
+    mov     ax,     es      ; ax = segment of PXENV+ structure
+    movzx   eax,    ax      ; eax = segment PXENV+ structure
+    shl     eax,    4       ; eax = PXENV+ structure * 16
+    movzx   ebx,    bx      ; ebx = offset PXENV+ structure
+    add     eax,    ebx     ; eax = linear address PXENV+ structure
+    push    eax             ; save PXENV+ structure address on stack   
+
     mov     ax,     cs
     mov     ds,     ax
     mov     es,     ax
     mov     fs,     ax
     mov     gs,     ax
 
-    mov     si,     hello
-    call    putstr
+    mov     si,     strings.hello
+    call    putstr    
+
+pmode_start:
+
+    push    dword desctbl.begin
+    push    word (desctbl.end - desctbl.begin)
+    mov     bp,     sp
+    lgdt    [bp]
+    add     sp,     6
+
+    cli     
+    call    non_maskable_irq_off
     
-    cli
-    hlt
+    mov     eax,    cr0
+    or      eax,    1
+    mov     cr0,    eax
 
-start_pmode:
-    cli 
+    jmp     0x0008:$+5
 
+use32
 
+    mov     ax,     ss
+    movzx   eax,    ax
+    shl     eax,    4
+    add     esp,    eax
 
-include "nmi.s"
-include "print.s"
-load_gdtr:
-    dw desctbl_end - desctbl
-    dd desctbl
+    call    start_32bit_code
+    call    reboot_via_8042
 
-desctbl:
-    dq 0x0000000000000000 ; 0x0000 = null
-    dq 0x00cf9e000000ffff ; 0x0008 = 32bit code 
-    dq 0x00cf92000000ffff ; 0x0010 = 32bit data/stack
-    dq 0x00009e000000ffff ; 0x0018 = 16bit code
-    dq 0x000092000000ffff ; 0x0020 = 16bit data/stack
-desctbl_end:
+    include "reboot.s"
 
-hello:
-    db      "Hello, world!",13,10,0
+    times   (16 - $ mod 16) nop
 
-    times   (512 - $ mod 512) nop
-
-start_32:
+start_32bit_code:
