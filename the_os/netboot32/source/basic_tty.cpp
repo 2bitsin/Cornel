554857a1c6e5d6331f065b025ac2c315fdf938de
Comrade Alex <s.aleksandras@gmail.com>
2022-08-25 16:48:02 +0300

    ...

--- the_os/netboot32/source/basic_tty.cpp
+++ the_os/netboot32/source/basic_tty.cpp
@@ -10,16 +10,20 @@ struct basic_tty
   std::uint16_t cursor_y;
   std::uint8_t  attribute;
   std::uint32_t page_offset;
+  std::uint16_t *buffer;
 
-  basic_tty()
-  {
-    
-  }
+  basic_tty() : 
+    page_columns { BDA_number_of_columns },
+    page_rows    { BDA_size_of_video_page / (page_columns * 2) },
+    cursor_x     { BDA_page_cursor_position[BDA_active_video_page] >> 8 },
+    cursor_y     { BDA_page_cursor_position[BDA_active_video_page] & 0xFF },
+    attribute    { 0x07 },
+    page_offset  { BDA_offset_of_video_page },
+    buffer       { (std::uint16_t*)0xB8000 }
+  {}
 
   ~basic_tty()
-  {
-    
-  }
+  {}
 };
 
 static basic_tty basic_tty_instance;
---
+++ the_os/netboot32/source/isr_handler.cpp
@@ -0,0 +1,6 @@
+#include <isr_handler.hpp>
+
+extern "C"
+__attribute__((cdecl))
+void ISR_handler(ISR_stack_frame& stack_frame)
+{}
