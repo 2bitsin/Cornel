0d51fbbc95d2d21d7713e3ff9fc4c85804ae87cb
Comrade Alex <s.aleksandras@gmail.com>
2022-11-05 23:52:37 +0200

    ...

--- the_os/library/vfsio/source/vfsio/vfsio.hpp
+++ the_os/library/vfsio/source/vfsio/vfsio.hpp
@@ -1,5 +1,69 @@
 #pragma once
 
-#include <vfsio/ibase.hpp>
-#include <vfsio/iblock.hpp>
-#include <vfsio/ivolume.hpp>
+#include <cstddef>
+#include <cstdint>
+
+#include <string_view>
+
+#include <memory>
+
+namespace vfsio
+{
+#if 0
+	struct Node
+	{
+		virtual ~Node () = default;
+	};
+	
+	struct DirectoryEntryNode:
+		public Node
+	{
+		virtual auto name() const -> std::string_view = 0;
+	};
+
+	template <typename T>
+	struct IteratorNode:
+		public Node
+	{
+		virtual auto next () -> bool = 0;
+		virtual auto get  () const -> std::unique_ptr<T> = 0;
+		virtual auto end  () const -> bool = 0;
+	};
+
+
+	struct iterator_end
+	{
+				
+	};
+	
+	template <typename T>
+	struct iterator
+	{	
+		iterator(std::unique_ptr<IteratorNode<T>> node) 
+		:	m_node (std::move(node))
+		{}
+
+		auto operator++ () -> iterator&
+		{
+			m_node->next();
+			return *this;
+		}
+
+		auto operator* () const 
+			-> decltype(m_node->get())
+		{
+			return m_node->get();
+		}
+
+	private:
+		std::unique_ptr<IteratorNode<T>> m_node;
+	};
+
+	struct DirectoryNode:
+		public Node
+	{
+		virtual auto begin () -> iterator<DirectoryEntryNode> = 0;
+		virtual auto end   () -> iterator_end = 0;	
+	};	
+#endif
+}
--- the_os/library/vfsio/tests/t000_vfsio_vfat_mount.cpp
+++ the_os/library/vfsio/tests/t000_vfsio_vfat_mount.cpp
@@ -6,10 +6,6 @@
 
 #include <textio/format.hpp>
 
-#include <vfsio/vfat_volume.hpp>
-#include <vfsio/cfile_block.hpp>
-
-
 
 #ifdef TESTING
 #define main t000_vfsio_vfat_mount
@@ -28,21 +24,6 @@ try
 
   auto out_i = std::back_inserter(buffer);
 
-	vfsio::cfile_block img("000_fat12.img", "rb");
-	auto [error_v, volume_v] = vfsio::vfat_volume::mount (img);
-	if (volume_v != nullptr)
-	{
-		auto root_v = volume_v->open("");
-		
-		while (true)
-		{
-			std::byte buff[32];
-			auto s = root_v->read(buff);
-			std::printf("> %.*s\n", s, buff);
-			if (s == 0)
-				break;
-		}
-	}
 
 	
   return 0;
