a37a0ef812d4320b94456b3ade51b75633260756
Comrade Alex <s.aleksandras@gmail.com>
2022-12-05 15:02:48 +0200

    ...

--- the_os/library/hardware/source/x86/paging.cpp
+++ the_os/library/hardware/source/x86/paging.cpp
@@ -1,2 +1,4 @@
 #include <hardware/x86/paging.hpp>
 
+
+
--- the_os/library/vfsio/source/elf_load.cpp
+++ the_os/library/vfsio/source/elf_load.cpp
@@ -11,12 +11,15 @@ auto vfsio::elf::load(vfsio::error& error_v, memory::block_list& block_list_v, s
 		||is_none_of<ET_EXEC, ET_DYN>(ehdr_p->e_type) 
 		||ehdr_p->e_machine != EM_386 
 		||ehdr_p->e_version != EV_CURRENT
-		||ehdr_p->e_entry == 0u		
+		||ehdr_p->e_entry == 0u
+		||ehdr_p->e_phoff < sizeof(Elf32_Ehdr)
 		) 
 	{	
 		error_v = vfsio::error::not_supported;
 		return {};
 	}
+
+	const auto phdr_p = partial_clone_from_bytes<Elf32_Phdr>(bytes_v, ehdr_p->e_phoff);
 
 	return {};
 }
