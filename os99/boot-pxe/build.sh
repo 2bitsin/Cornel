53368af2d1a5895fcb277342799743c79d25dfba
alex <s.aleksandras@gmail.com>
2024-06-26 03:19:15 +0300

    ...

--- os99/boot-pxe/build.sh
+++ os99/boot-pxe/build.sh
@@ -1,14 +1,25 @@
-#!/bin/bash
+#!/bin/bash -l
+
 
 # local variables
-TEMPORARY_PATH=$WORKSPACE_ROOT/temp
+TEMPORARY_PATH=$WORKSPACE_ROOT/build/boot-pxe
 PREABLE_PATH=$TEMPORARY_PATH/preamble.bin
 TARGET_PATH=$WORKSPACE_ROOT/boot-pxe.sys
 CONFIG_PATH=$WORKSPACE_ROOT/config.ini
 
 mkdir -p $TEMPORARY_PATH
 
-fasm preamble/preamble.asm $PREABLE_PATH
+fasm bootstrap/main.asm $PREABLE_PATH
+
+cmake -B $TEMPORARY_PATH/boot-pxe         \
+  -G "Watcom WMake"                       \
+  -D CMAKE_INSTALL_PREFIX=$TEMPORARY_PATH \
+  -D CMAKE_BUILD_TYPE=$BUILD_TYPE         \
+  -D CMAKE_SYSTEM_NAME=DOS                \
+  -D CMAKE_SYSTEM_PROCESSOR=I86           \
+  -S source                  
+cmake --build   $TEMPORARY_PATH/boot-pxe --config $BUILD_TYPE
+cmake --install $TEMPORARY_PATH/boot-pxe 
 
 cp $PREABLE_PATH $TARGET_PATH
 cp ./config.ini $CONFIG_PATH
--- os99/build.sh
+++ os99/build.sh
@@ -3,5 +3,7 @@ docker run -it                                           \
   -v $PROJECT_ROOT:/os99                                 \
   -e PROJECT_ROOT=/os99                                  \
   -e WORKSPACE_ROOT=/os99/workspace                      \
+  -e BUILD_TYPE=$BUILD_TYPE                              \
+  -w /os99/os99/boot-pxe                                 \
   os99-ia16                                              \
-    /bin/bash -c 'cd /os99/os99/boot-pxe && ./build.sh'  
+  bash -c -l ./build.sh
