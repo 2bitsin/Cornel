1636396a738a88b23b2ed487bb52717c4dd60966
alex <s.aleksandras@gmail.com>
2024-06-27 18:57:26 +0300

    ...

--- os99/boot-pxe/bootstrap/main.asm
+++ os99/boot-pxe/bootstrap/main.asm
@@ -1,17 +1,26 @@
     format binary    
     use16
 
-    LOAD_ADDRESS equ 0x7c00
+    LOAD_ADDRESS 	equ 0x7c00
+		MZ_SEGMENT		equ gs
+
+		include "mzhead.asi"
 
     org     LOAD_ADDRESS
     jmp     preamble
 
 	header:
-		.version db "BOOT-PXE v0.1 (BUILD 2024-06-26)", 13, 10, 0		
+		.version:
+			db 		"BOOT-PXE v0.1", 13, 10, 0		
 
 	strings:
-		.int20h  db "int 0x20", 13, 10, 0
-		.int21h  db "int 0x21", 13, 10, 0
+		.int20h:
+			db 		"int 0x20", 13, 10, 0
+		.int21h:
+			db 		"int 0x21", 13, 10, 0
+		.corrupted:
+			db		"Bootloader corrupted, ", 13, 10
+			db 		"system halted.", 13, 10, 0
 
 		align 16			
 
@@ -33,10 +42,6 @@
 
 		call		coninit
 
-		mov     si,   header.version
-
-		call		putstr
-
 ; Setup INT 0x20
 		mov			ax,		cs
 		mov			[fs:0x20*4 + 0], word __dos_20h
@@ -45,11 +50,81 @@
 ; Setup INT 0x21 
 		mov			[fs:0x21*4 + 0], word __dos_21h
 		mov			[fs:0x21*4 + 2], ax
+
+; Parse MZ header
+
+		mov 		ax, 	__payload
+		shr			ax, 	4
+		mov			gs, 	ax
+
+		mov			ax,   MZ_SIGNATURE
+		cmp			ax, 	0x5A4D
+		jnz			.corrupted
+
+		mov			bx, 	MZ_RELOCOFFS		
+		mov			cx, 	MZ_RELOCSIZE
+
+		xchg		bx,		bx
+	.patch_reloc:
 		
+	; Compute the segment to be patched
+		mov			ax,		[MZ_SEGMENT:bx + 2]
+		add     ax,		__payload / 16
+		mov			fs, 	ax
+
+	; Get the offset to be patched
+		push		bx
+		mov			bx,		[MZ_SEGMENT:bx]
+
+
+	; Patch the relocation
+		mov			ax, 	[fs:bx]
+		add			ax,		word __payload / 16
+		mov			[fs:bx],ax
+
+	; Restore bx
+		pop			bx
+		add			bx,		2
+		dec			cx
+		jnz			.patch_reloc
+
+	; Done patching, setup environment
+		
+		mov			ax,		__PSP / 16
+		mov			ds,		ax
+		mov			es, 	ax
+		
+		mov			ax, 	MZ_INITIALSS
+		add			ax,		__payload / 16
+		mov			ss,		ax
+		mov			sp,		MZ_INITIALSP
+
+	; RETF to initial CS:IP
+		mov			ax, 	MZ_INITIALCS
+		add			ax, 	__payload / 16
+		push		ax
+		mov			ax, 	MZ_INITIALIP
+		push		ax
+		mov			ax,		0xffff
+
+		retf
+		
+
+	; .....
+		mov     si,   header.version
+		call		putstr
+		cli
+		hlt
+
+	.corrupted:
+		xchg		bx,		bx
+		mov			si, 	strings.corrupted
+		call		putstr
 
 		cli
 		hlt
 
+	
 ; Dummy INT 0x20
 	__dos_20h:
 
@@ -106,13 +181,69 @@
 
 		iret
 
+		align 	256
+	__PSP:
+		.exit_vector:	
+			dw		0
+		.mem_top_seg:
+			dw		0
+		; reserved
+			db		0
+		; ??????
+			db    0
+		.com_seg_size:
+			dw 		0
+		; ?????
+			dw		0
+		.exit_addr:
+			dd		0
+		.cbrk_addr:
+			dd 		0
+		.cerr_addr:
+			dd		0
+		.parent_psp_seg:
+			dw		0
+		.job_file_tbl:
+			times 20 db 0
+		.env_seg:
+			dw 		0
+		.temp_ss_sp:
+			dd		0
+		.jft_size:
+			dw		0
+		.jft_pointer:
+			dd		0
+		.prev_psp_ptr:
+			dd		0
+		; reserved
+			dd		0
+		.dos_version:
+			dw		0x610
+		; reserved
+			times 14 db 0
+		.call_far_dos:
+			int		0x21
+			retf
+		; reserved
+			dw 		0
+		; reserved, fcb?
+			dd		0
+			dw 		0
+			db		0
+		.fcbs_1:
+			times	16 db 0
+		.fcbs_2:
+			times 20 db 0
+
+		.cmdline_size:
+			db		0
+		.cmdline_str:
+			times 127 db 0xd
 
 	PADSIZE   equ   512
-	PADDING   equ 	times ((PADSIZE - (($ - $$) mod PADSIZE)) - 2)
-	PADDING 	db    0
-		        dw    0xAA55
-	
-
+	PADDING   equ 	times ((PADSIZE - (($ - $$) mod PADSIZE)))
+	PADDING 	db    1	
+		align		16
 	__payload:
 
 
--- os99/boot-pxe/build.sh
+++ os99/boot-pxe/build.sh
@@ -14,11 +14,12 @@ fasm bootstrap/main.asm $PREABLE_PATH
 
 cmake -B $TEMPORARY_PATH/boot-pxe         \
   -G "Watcom WMake"                       \
+  -D CMAKE_VERBOSE_MAKEFILE=YES           \
   -D CMAKE_INSTALL_PREFIX=$TEMPORARY_PATH \
   -D CMAKE_BUILD_TYPE=$BUILD_TYPE         \
   -D CMAKE_SYSTEM_NAME=DOS                \
   -D CMAKE_SYSTEM_PROCESSOR=I86           \
-  -S source                  
+  -S source    
 cmake --build   $TEMPORARY_PATH/boot-pxe --config $BUILD_TYPE
 cmake --install $TEMPORARY_PATH/boot-pxe 
 
