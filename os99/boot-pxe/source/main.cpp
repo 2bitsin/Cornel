6ebb865ffd7d981efff8a5d532f9d46db2c79e7b
alex <s.aleksandras@gmail.com>
2024-07-04 01:03:34 +0300

    ...

--- os99/boot-pxe/source/main.cpp
+++ os99/boot-pxe/source/main.cpp
@@ -3,15 +3,36 @@
 #include <stddef.h>
 #include <string.h>
 #include <stdlib.h>
+#include <malloc.h>
 
 #include "x86/asm.h"
 #include "pxe/pxe.h"
 
+static PXE_bootph_type __far s_bootph_reply;
+static size_t __far s_size;
 
 int main(int, char**) 
 {       
   puts("BOOT-PXE v0.1 (BUILD: " __TIME__ " " __DATE__ ")");
-  PXE_init();
+  if (PXE_init() != success) {
+    puts("PXE not available or unusable.");
+    return -1;
+  }
+  PXE_print_info();
+  s_size = sizeof(PXE_bootph_type);
+  _fmemset(&s_bootph_reply, 0, sizeof(PXE_bootph_type));    
+  if (PXE_get_cached_info(cached_reply, &s_bootph_reply, &s_size) != success) {
+    puts("Failed to get PXE info.");
+    return -1;
+  }
+  
+  puts(" --- PXE BOOTP ---");
+  printf("Client IP ....... : %d.%d.%d.%d\n", s_bootph_reply.cip.value_u8[0], s_bootph_reply.cip.value_u8[1], s_bootph_reply.cip.value_u8[2], s_bootph_reply.cip.value_u8[3]);
+  printf("Your IP ......... : %d.%d.%d.%d\n", s_bootph_reply.yip.value_u8[0], s_bootph_reply.yip.value_u8[1], s_bootph_reply.yip.value_u8[2], s_bootph_reply.yip.value_u8[3]);
+  printf("Your MAC ........ : %02X:%02X:%02X:%02X:%02X:%02X\n", s_bootph_reply.CAddr[0], s_bootph_reply.CAddr[1], s_bootph_reply.CAddr[2], s_bootph_reply.CAddr[3], s_bootph_reply.CAddr[4], s_bootph_reply.CAddr[5]);
+  printf("Server IP ....... : %d.%d.%d.%d\n", s_bootph_reply.sip.value_u8[0], s_bootph_reply.sip.value_u8[1], s_bootph_reply.sip.value_u8[2], s_bootph_reply.sip.value_u8[3]);
+  printf("Gateway IP ...... : %d.%d.%d.%d\n", s_bootph_reply.gip.value_u8[0], s_bootph_reply.gip.value_u8[1], s_bootph_reply.gip.value_u8[2], s_bootph_reply.gip.value_u8[3]);        
   return 0;
 }
+
 
--- os99/boot-pxe/source/pxe/pxe.cpp
+++ os99/boot-pxe/source/pxe/pxe.cpp
@@ -4,6 +4,9 @@
 #include <stdio.h>
 #include <string.h>
 
+static PXENVplus far* s_PXENVplus;
+static PXEbang far* s_PXEbang;
+
 bool PXE_checksum(void far* data, size_t size) 
 {
   uint8_t sum = 0;
@@ -12,38 +15,73 @@ bool PXE_checksum(void far* data, size_t size)
   return sum == 0;
 }
 
-uint16_t PXE_init() 
+void PXE_print_info()
 {
-  PXENVplus far* pxep;
-  REGPACK r;
-  
+  puts(" --- PXENV+ ---");
+
+  printf("PXENV+ Version ... : v%d.%d\n", s_PXENVplus->Version >> 8, s_PXENVplus->Version & 0xFF);
+  printf("Length ........... : %d\n", s_PXENVplus->Length);
+  printf("Checksum ......... : %d\n", s_PXENVplus->Checksum);
+  printf("RMEntry .......... : %04X:%04X\n", s_PXENVplus->RMEntry.seg, s_PXENVplus->RMEntry.off);
+  printf("PMOffset ......... : %08X\n", s_PXENVplus->PMOffset);
+  printf("PMSelector ....... : %04X\n", s_PXENVplus->PMSelector);
+  printf("StackSeg ......... : %04X\n", s_PXENVplus->StackSeg);
+  printf("StackSize ........ : %04X\n", s_PXENVplus->StackSize);
+  printf("BC_CodeSeg ....... : %04X\n", s_PXENVplus->BC_CodeSeg);
+  printf("BC_CodeSize ...... : %04X\n", s_PXENVplus->BC_CodeSize);
+  printf("BC_DataSeg ....... : %04X\n", s_PXENVplus->BC_DataSeg);
+  printf("BC_DataSize ...... : %04X\n", s_PXENVplus->BC_DataSize);
+  printf("UNDIDataSeg ...... : %04X\n", s_PXENVplus->UNDIDataSeg);
+  printf("UNDIDataSize ..... : %04X\n", s_PXENVplus->UNDIDataSize);
+  printf("UNDICodeSeg ...... : %04X\n", s_PXENVplus->UNDICodeSeg);
+  printf("UNDICodeSize ..... : %04X\n", s_PXENVplus->UNDICodeSize);
+  printf("PXEPtr ........... : %04X:%04X\n", s_PXENVplus->PXEPtr->UNDIROMID.seg, s_PXENVplus->PXEPtr->UNDIROMID.off);  
+
+  puts(" --- PXE! ---");
+
+  printf("Signature ........ : %c%c%c%c\n", s_PXEbang->Signature[0], s_PXEbang->Signature[1], s_PXEbang->Signature[2], s_PXEbang->Signature[3]);
+  printf("StructLength ..... : %d\n", s_PXEbang->StructLength);
+  printf("StructCksum ...... : %d\n", s_PXEbang->StructCksum);
+  printf("StructRev ........ : %d\n", s_PXEbang->StructRev);
+  printf("UNDIROMID ........ : %04X:%04X\n", s_PXEbang->UNDIROMID.seg, s_PXEbang->UNDIROMID.off);
+  printf("BaseROMID ........ : %04X:%04X\n", s_PXEbang->BaseROMID.seg, s_PXEbang->BaseROMID.off);
+  printf("EntryPointSP ..... : %04X:%04X\n", s_PXEbang->EntryPointSP, s_PXEbang->EntryPointSP);
+  printf("EntryPointESP .... : %04X:%04X\n", s_PXEbang->EntryPointESP.seg, s_PXEbang->EntryPointESP.off);
+  printf("StatusCallout .... : %04X:%04X\n", s_PXEbang->StatusCallout.seg, s_PXEbang->StatusCallout.off);
+  printf("SegDescCnt ....... : %d\n", s_PXEbang->SegDescCnt);
+  printf("FirstSelector .... : %04X\n", s_PXEbang->FirstSelector);
+  printf("Stack ............ : %04X:%08X:%04X\n", s_PXEbang->Stack.segment_address, s_PXEbang->Stack.physical_address, s_PXEbang->Stack.segment_length);
+  printf("UNDIData ......... : %04X:%08X:%04X\n", s_PXEbang->UNDIData.segment_address, s_PXEbang->UNDIData.physical_address, s_PXEbang->UNDIData.segment_length);
+  printf("UNDICode ......... : %04X:%08X:%04X\n", s_PXEbang->UNDICode.segment_address, s_PXEbang->UNDICode.physical_address, s_PXEbang->UNDICode.segment_length);
+  printf("UNDICodeWrite .... : %04X:%08X:%04X\n", s_PXEbang->UNDICodeWrite.segment_address, s_PXEbang->UNDICodeWrite.physical_address, s_PXEbang->UNDICodeWrite.segment_length);  
+}
+
+PXE_status PXE_init() 
+{  
+  REGPACK r;  
   r.w.ax = 0x5650;
   intr(0x1A, &r);
   if (r.w.ax != 0x564E || (r.w.flags & 1))
-    return 0xffff;
-  pxep = (PXENVplus far*)MK_FP(r.w.es, r.w.bx);
-  if (!PXE_checksum(pxep, pxep->Length))
-    return 0xffff;
-  if (_fmemcmp(pxep->Signature, "PXENV+", 6) != 0)
-    return 0xffff;  
+    return pxe_not_available;
+  s_PXENVplus = (PXENVplus far*)MK_FP(r.w.es, r.w.bx);
+  if (!PXE_checksum(s_PXENVplus, s_PXENVplus->Length))
+    return pxenv_bad_checksum;
+  if (_fmemcmp(s_PXENVplus->Signature, "PXENV+", 6) != 0)
+    return pxenv_bad_signature;
+  if (s_PXENVplus->Version < 0x0201)
+    return version_not_supported;
+  s_PXEbang = s_PXENVplus->PXEPtr;
+  return success;
+}
 
-  printf("PXE-VERSION .... : %d.%d\n", pxep->Version >> 8, pxep->Version & 0xff);
-  printf("RMEntry ........ : %04X:%04X\n", pxep->RMEntry.seg, pxep->RMEntry.off);
-  printf("PMOffset ....... : %08X\n", pxep->PMOffset);
-  printf("PMSelector ..... : %04X\n", pxep->PMSelector);
-  printf("StackSeg ....... : %04X\n", pxep->StackSeg);
-  printf("StackSize ...... : %04X\n", pxep->StackSize);
-  printf("BC_CodeSeg ..... : %04X\n", pxep->BC_CodeSeg);
-  printf("BC_CodeSize .... : %04X\n", pxep->BC_CodeSize);
-  printf("BC_DataSeg ..... : %04X\n", pxep->BC_DataSeg);
-  printf("BC_DataSize .... : %04X\n", pxep->BC_DataSize);
-  printf("UNDIDataSeg .... : %04X\n", pxep->UNDIDataSeg);
-  printf("UNDIDataSize ... : %04X\n", pxep->UNDIDataSize);
-  printf("UNDICodeSeg .... : %04X\n", pxep->UNDICodeSeg);
-  printf("UNDICodeSize ... : %04X\n", pxep->UNDICodeSize);
-  printf("PXEPtr ......... : %Fp\n", pxep->PXEPtr);
-
-
- 
-  return pxep->Version;
+PXE_status PXE_get_cached_info(PXENV_packet_type packet_type, PXE_bootph_type far* info_buff, size_t far* info_size)
+{
+  static PXENV_get_cached_info_type __far v_params;
+  v_params.PacketType = packet_type;
+  v_params.Buffer = info_buff;
+  v_params.BufferSize = *info_size;
+  s_PXEbang->EntryPointSP(0x71u, &v_params);
+  if (v_params.Status == success) {    
+    *info_size = v_params.BufferSize; }
+  return (PXE_status)v_params.Status;
 }
