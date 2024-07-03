df5ba42db303a3dbb3439fea9d7e4518547f1ae3
alex <s.aleksandras@gmail.com>
2024-07-03 15:55:53 +0300

    ...

--- os99/boot-pxe/source/main.c
+++ os99/boot-pxe/source/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stddef.h>
+#include <string.h>
 
 #include "doshelp.h"
 #include "asm.h"
@@ -10,12 +11,19 @@ extern  char    **environ;
 int main(int argc, char** argv) 
 {       
   int i ;
+  char* s_name;
   for(i = 0; i < argc; ++i) {
     printf("ARG[%i] = \"%s\"\n", i, argv[i]);
   }
   for(i = 0; environ[i]!=0; ++i) {
     printf("ENV[%i] = \"%s\"\n", i, environ[i]);
   }
+
+  s_name = strdup("World");
+  printf("Hello %s [%04x]!\n", s_name, (uint16_t)s_name);
+  free(s_name);
+
+
   return 0;
 }
 
